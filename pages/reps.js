fc9eb0b9afeadffa877eef013f83055113c9a843
brainless <46744361+brainless-afk@users.noreply.github.com>
2023-10-02 22:56:44 +0200

    formatting

--- pages/reps.js
+++ pages/reps.js
@@ -1,88 +1,123 @@
-import Head from "next/head"
-import Link from "next/link"
-import React, { useContext } from "react"
-import useSWR from 'swr'
-import { API_ROUTES, API_EPOCH } from '../common/enums'
-import { CommonFooter, CommonMetadata } from "../components/page_meta"
-import { VideoBox } from '../components/video_box'
-import { LangContext, useDictionary, useLocalizationForRootComponentsOnly } from "../lang/dict_manager"
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import React, { useContext } from "react";
+import useSWR from "swr";
+import { API_ROUTES, API_EPOCH } from "../common/enums";
+import { CommonFooter, CommonMetadata } from "../components/page_meta";
+import { VideoBox } from "../components/video_box";
+import {
+  LangContext,
+  useDictionary,
+  useLocalizationForRootComponentsOnly,
+} from "../lang/dict_manager";
+import styles from "../styles/Home.module.css";
 
 export async function getServerSideProps({ req, res, query }) {
-    const ds = await import("../server/data_sources")
-    const coordinator = await ds.getDatabase()
-    
-    const vodInfo = await coordinator.getVod()
-    await coordinator.teardown()
-    
-    if (vodInfo) {
-        return {
-            props: {
-                info: {
-                    link: `https://www.youtube.com/watch?v=${vodInfo.video_link}`,
-                    title: vodInfo.title,
-                    thumbnail: vodInfo.thumbnail
-                },
-                uploadDate: vodInfo.uploaded_date,
-                channelLink: `https://www.youtube.com/channel/${process.env.WATCH_YT_CHANNEL_ID}`
-            }
-        }
-    } else {
-        res.statusCode = 503
-        return {
-            props: {
-                error: "NO_VIDEO_FOUND",
-                channelLink: `https://www.youtube.com/channel/${process.env.WATCH_YT_CHANNEL_ID}`
-            }
-        }
-    }
+  const ds = await import("../server/data_sources");
+  const coordinator = await ds.getDatabase();
+
+  const vodInfo = await coordinator.getVod();
+  await coordinator.teardown();
+
+  if (vodInfo) {
+    return {
+      props: {
+        info: {
+          link: `https://www.youtube.com/watch?v=${vodInfo.video_link}`,
+          title: vodInfo.title,
+          thumbnail: vodInfo.thumbnail,
+        },
+        uploadDate: vodInfo.uploaded_date,
+        channelLink: `https://www.youtube.com/channel/${process.env.WATCH_YT_CHANNEL_ID}`,
+      },
+    };
+  } else {
+    res.statusCode = 503;
+    return {
+      props: {
+        error: "NO_VIDEO_FOUND",
+        channelLink: `https://www.youtube.com/channel/${process.env.WATCH_YT_CHANNEL_ID}`,
+      },
+    };
+  }
 }
 
 function VodInfo(props) {
-    const lang = useContext(LangContext)
-    const date = new Date(props.uploadDate)
-    const caption = lang.formatString(lang.Reps.VodInfoUploadDate, useDictionary().FormatDateShort(date))
+  const lang = useContext(LangContext);
+  const date = new Date(props.uploadDate);
+  const caption = lang.formatString(
+    lang.Reps.VodInfoUploadDate,
+    useDictionary().FormatDateShort(date)
+  );
 
-    return <div className={styles.streamInfo}>
-        <VideoBox info={props.info} caption={caption} />
+  return (
+    <div className={styles.streamInfo}>
+      <VideoBox info={props.info} caption={caption} />
     </div>
+  );
 }
 
 export default function Reps(props) {
-    const { data, isValidating, mutate } = useSWR(API_ROUTES.RANDOM_VOD, (url) => fetch(url).then(r => r.json()), {
-        fallbackData: {info: props.info, uploadDate: props.uploadDate, error: props.error},
-        revalidateOnFocus: false,
-        revalidateOnMount: false,
-        revalidateOnReconnect: false,
-        revalidateIfStale: false
-    })
-    const lang = useLocalizationForRootComponentsOnly()
-
-    if (typeof window !== "undefined" && data.serverVersion > API_EPOCH) {
-        window.location.reload()
+  const { data, isValidating, mutate } = useSWR(
+    API_ROUTES.RANDOM_VOD,
+    (url) => fetch(url).then((r) => r.json()),
+    {
+      fallbackData: {
+        info: props.info,
+        uploadDate: props.uploadDate,
+        error: props.error,
+      },
+      revalidateOnFocus: false,
+      revalidateOnMount: false,
+      revalidateOnReconnect: false,
+      revalidateIfStale: false,
     }
+  );
+  const lang = useLocalizationForRootComponentsOnly();
 
-    return <LangContext.Provider value={lang}>
-        <div className={styles.site}>
-            <CommonMetadata />
-            <Head>
-                <title>{lang.Reps.PageTitle}</title>
-                <meta content={lang.Reps.SMMetaDescription} property="og:description" />
-            </Head>
-            <div className={`${styles.site} ${styles.repsPage} ${isValidating? styles.isReloading : ''}`}>
-                <p className={styles.bareTextContainer}>{lang.Reps.PageCaption}</p>
-                {data.error
-                    ? <div className={`${styles.streamInfo} ${styles.streamInfoError}`}>
-                        <p>{lang.formatString(lang.Reps.ErrorDescription, lang.Reps.ErrorCodes[data.error])}</p>
-                    </div> 
-                    : <VodInfo info={data.info} uploadDate={data.uploadDate} />}
-                <button className={styles.bigButton} onClick={() => mutate()}>
-                    {lang.Reps.RerollButton}
-                </button>
-                
-                <p><Link href="/">{lang.Reps.BackToStreamTrackerButton}</Link></p>
-                <CommonFooter channelLink={props.channelLink} />
+  if (typeof window !== "undefined" && data.serverVersion > API_EPOCH) {
+    window.location.reload();
+  }
+
+  return (
+    <LangContext.Provider value={lang}>
+      <div className={styles.site}>
+        <CommonMetadata />
+        <Head>
+          <title>{lang.Reps.PageTitle}</title>
+          <meta
+            content={lang.Reps.SMMetaDescription}
+            property="og:description"
+          />
+        </Head>
+        <div
+          className={`${styles.site} ${styles.repsPage} ${
+            isValidating ? styles.isReloading : ""
+          }`}
+        >
+          <p className={styles.bareTextContainer}>{lang.Reps.PageCaption}</p>
+          {data.error ? (
+            <div className={`${styles.streamInfo} ${styles.streamInfoError}`}>
+              <p>
+                {lang.formatString(
+                  lang.Reps.ErrorDescription,
+                  lang.Reps.ErrorCodes[data.error]
+                )}
+              </p>
             </div>
+          ) : (
+            <VodInfo info={data.info} uploadDate={data.uploadDate} />
+          )}
+          <button className={styles.bigButton} onClick={() => mutate()}>
+            {lang.Reps.RerollButton}
+          </button>
+
+          <p>
+            <Link href="/">{lang.Reps.BackToStreamTrackerButton}</Link>
+          </p>
+          <CommonFooter channelLink={props.channelLink} />
         </div>
+      </div>
     </LangContext.Provider>
+  );
 }
--- tool/updateVODList.js
+++ tool/updateVODList.js
@@ -1,35 +1,36 @@
-require('dotenv').config()
-const process = require("process")
-const fetch = require("node-fetch")
-const sqlite3 = require("sqlite3")
-const decode = require("html-entities").decode
-const pgClient = require("pg").Client
+require("dotenv").config();
+const process = require("process");
+const fetch = require("node-fetch");
+const sqlite3 = require("sqlite3");
+const decode = require("html-entities").decode;
+const pgClient = require("pg").Client;
 
 class PostgresDatabase {
-    constructor() {
-        this.connection = null
+  constructor() {
+    this.connection = null;
+  }
+
+  async _connect() {
+    for (let i = 0; i < 3; ++i) {
+      try {
+        this.connection = new pgClient({ connectionTimeoutMillis: 5000 });
+        await this.connection.connect();
+        return this;
+      } catch (e) {
+        console.error("[_connect]", "error connecting", e);
+      }
     }
-    
-    async _connect() {
-        for (let i = 0; i < 3; ++i) {
-            try {
-                this.connection = new pgClient({connectionTimeoutMillis: 5000})
-                await this.connection.connect()
-                return this
-            } catch (e) {
-                console.error("[_connect]", "error connecting", e)
-            }
-        }
-    
-        throw new Error("[_connect]: all connection attempts failed")
-    }
-    
-    async insertVods(vodEntries) {
-        const ts = Date.now()
-        console.debug("[insertVod]", "enter")
-        await this.transaction(async (client) => {
-            for (let vod of vodEntries) {
-                await client.query(`
+
+    throw new Error("[_connect]: all connection attempts failed");
+  }
+
+  async insertVods(vodEntries) {
+    const ts = Date.now();
+    console.debug("[insertVod]", "enter");
+    await this.transaction(async (client) => {
+      for (let vod of vodEntries) {
+        await client.query(
+          `
                     INSERT INTO vod VALUES ($1, $2, $3, $4, $5, false, $6)
                     ON CONFLICT (video_link) DO UPDATE SET
                         video_link             = excluded.video_link,
@@ -39,38 +40,47 @@ class PostgresDatabase {
                         length_seconds         = excluded.length_seconds,
                         members_only           = excluded.members_only,
                         _last_valid            = excluded._last_valid
-                `, [vod.videoID, vod.title, vod.thumbnail, vod.uploaded_date, vod.length_seconds, ts])
-            }
-        })
-        console.debug("[insertVod]", "exit")
+                `,
+          [
+            vod.videoID,
+            vod.title,
+            vod.thumbnail,
+            vod.uploaded_date,
+            vod.length_seconds,
+            ts,
+          ]
+        );
+      }
+    });
+    console.debug("[insertVod]", "exit");
+  }
+
+  async transaction(f) {
+    let retVal;
+    try {
+      await this.connection.query("BEGIN");
+      retVal = await f(this.connection);
+      await this.connection.query("COMMIT");
+    } catch (e) {
+      await this.connection.query("ROLLBACK");
+      console.error("[transaction]", "query error:", e);
     }
-    
-    async transaction(f) {
-        let retVal
-        try {
-            await this.connection.query("BEGIN")
-            retVal = await f(this.connection)
-            await this.connection.query("COMMIT")
-        } catch (e) {
-            await this.connection.query("ROLLBACK")
-            console.error("[transaction]", "query error:", e)
-        }
-        return retVal
-    }
-    
-    async teardown() {
-        console.debug("[teardown]", "closing connection now")
-        await this.connection.end()
-    }   
+    return retVal;
+  }
+
+  async teardown() {
+    console.debug("[teardown]", "closing connection now");
+    await this.connection.end();
+  }
 }
 
 class SQLiteDatabase {
-    constructor() {
-        this.connection = new sqlite3.Database(process.env.SQLITE_DB_PATH)
-    }
+  constructor() {
+    this.connection = new sqlite3.Database(process.env.SQLITE_DB_PATH);
+  }
 
-    async insertVods(vodEntries) {
-        const stmt = this.connection.prepare(`
+  async insertVods(vodEntries) {
+    const stmt = this.connection.prepare(`
             INSERT INTO vod VALUES (?, ?, ?, ?, ?, 0, 0)
             ON CONFLICT (video_link) DO UPDATE SET
                 video_link          = excluded.video_link,
@@ -80,72 +90,76 @@ class SQLiteDatabase {
                 length_seconds      = excluded.length_seconds,
                 members_only        = excluded.members_only,
                 _last_valid         = excluded._last_valid
-        `)
+        `);
 
-        vodEntries.forEach((v) => 
-            stmt.run([
-                v.videoID, v.title, v.thumbnail, v.uploaded_date, v.length_seconds
-            ])
-        )
-        stmt.finalize()
-    }
+    vodEntries.forEach((v) =>
+      stmt.run([
+        v.videoID,
+        v.title,
+        v.thumbnail,
+        v.uploaded_date,
+        v.length_seconds,
+      ])
+    );
+    stmt.finalize();
+  }
 
-    async teardown() {
-        this.connection.close()
-    }
+  async teardown() {
+    this.connection.close();
+  }
 }
 
 function makeAPIURL(pageToken) {
-    const apiKey = process.env.YOUTUBE_API_KEY
-    const channelID = process.env.WATCH_YT_CHANNEL_ID
-    let pageSpecifier = "&"
-    if (pageToken) {
-        pageSpecifier = `pageToken=${pageToken}&`
-    }
-    
-    return `https://youtube.googleapis.com/youtube/v3/search?${pageSpecifier}part=snippet&channelId=${channelID}&maxResults=500&order=date&type=video&key=${apiKey}`
+  const apiKey = process.env.YOUTUBE_API_KEY;
+  const channelID = process.env.WATCH_YT_CHANNEL_ID;
+  let pageSpecifier = "&";
+  if (pageToken) {
+    pageSpecifier = `pageToken=${pageToken}&`;
+  }
+
+  return `https://youtube.googleapis.com/youtube/v3/search?${pageSpecifier}part=snippet&channelId=${channelID}&maxResults=500&order=date&type=video&key=${apiKey}`;
 }
 
 function collect(item) {
-    return {
-        videoID: item.id.videoId,
-        title: decode(item.snippet.title),
-        thumbnail: item.snippet.thumbnails.medium.url,
-        uploaded_date: Date.parse(item.snippet.publishedAt),
-        length_seconds: 0
-    }
+  return {
+    videoID: item.id.videoId,
+    title: decode(item.snippet.title),
+    thumbnail: item.snippet.thumbnails.medium.url,
+    uploaded_date: Date.parse(item.snippet.publishedAt),
+    length_seconds: 0,
+  };
 }
 
 async function main() {
-    let db
-    if (process.env.DATABASE_TYPE === "sqlite3") {
-        db = new SQLiteDatabase()
+  let db;
+  if (process.env.DATABASE_TYPE === "sqlite3") {
+    db = new SQLiteDatabase();
+  } else {
+    db = await new PostgresDatabase()._connect();
+  }
+
+  let nextUrl = makeAPIURL();
+  while (nextUrl) {
+    const resp = await fetch(nextUrl);
+    if (resp.status !== 200) {
+      console.error("code:", resp.status);
+      console.error(await resp.json());
+      break;
+    }
+
+    const data = await resp.json();
+    await db.insertVods(data.items.map((video) => collect(video)));
+
+    if (data.nextPageToken) {
+      nextUrl = makeAPIURL(data.nextPageToken);
     } else {
-        db = await (new PostgresDatabase())._connect()
+      break;
     }
-    
-    let nextUrl = makeAPIURL()
-    while (nextUrl) {
-        const resp = await fetch(nextUrl)
-        if (resp.status !== 200) {
-            console.error("code:", resp.status)
-            console.error(await resp.json())
-            break
-        }
-        
-        const data = await resp.json()
-        await db.insertVods(data.items.map((video) => collect(video)))
-        
-        if (data.nextPageToken) {
-            nextUrl = makeAPIURL(data.nextPageToken)
-        } else {
-            break
-        }
-    }
-    
-    await db.teardown()
+  }
+
+  await db.teardown();
 }
 
 main().then(() => {
-    process.exit(0)
-})
+  process.exit(0);
+});
